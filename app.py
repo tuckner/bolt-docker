c436e392cea6a31a38ed7f3e627765e7d7186a64
tuckner <tuckner329@gmail.com>
2022-06-27 09:16:19 -0500

    slash command handling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import requests
+import re
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -10,6 +11,13 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 def send_event(event):
   print('[LOG] Message received: {}'.format(event))
   requests.post(os.environ["WEBHOOK_ADDRESS"], json=event)
+
+@app.command(re.compile(".*"))
+def send_command(ack, respond, command):
+  print('[LOG] Message received: {}'.format(command))
+  ack()
+  respond(f"Got it!")
+  requests.post(os.environ["WEBHOOK_ADDRESS"], json=command)
 
 # Start your app
 if __name__ == "__main__":
